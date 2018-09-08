3efb53a7b2711504650ab91f0b9b5b313b44cc09
Hesam Talebi <5802354+hesamwls@users.noreply.github.com>
2018-09-08 13:14:29 +0430

    faq section added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
 console.log('\'Allo \'Allo!');
+
+[].forEach.call(document.querySelectorAll('.collapsible a'),function(a){
+    a.addEventListener('click',function(){
+        var li = a.parentElement;
+        if(li.classList.contains('selected'))
+            li.classList.remove('selected');
+        else
+            li.classList.add('selected');
+        var nextLi = li;
+        var prevLi = li;
+        while(nextLi=nextLi.nextSibling){if(nextLi.nodeName.toLowerCase() == 'li') nextLi.classList.remove('selected');}
+        while(prevLi=prevLi.previousSibling){if(prevLi.nodeName.toLowerCase() == 'li') prevLi.classList.remove('selected');}
+    });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('html', ['styles', 'scripts'], () => {
   return gulp.src('app/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
-    .pipe($.if(/\.css$/, $.cssnano({safe: true, autoprefixer: false})))
+    .pipe($.if(/\.css$/, $.cssnano({safe: true, autoprefixer: true})))
     .pipe($.if(/\.html$/, $.htmlmin({
       collapseWhitespace: true,
       minifyCSS: true,
