0742d21939ebf28fba8391bdd4c6fd9d9e068246
Hesam Talebi <5802354+hesamwls@users.noreply.github.com>
2018-10-31 03:02:25 +0330

    minor improvementss

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,8 +3,14 @@ $('.collapsible a').on('click',function(a){
     li.toggleClass('selected');
     li.siblings().removeClass('selected');
 });
+$('.subscription-btn').on('click',function(){
+    gtag('event', 'subscribed', {
+        'event_category': 'Buttons',
+        'event_label': 'Subscription Button'
+      });   
+});
 window.ml_webform_after_success = function(){
     toastr.options.positionClass='toast-bottom-right';
-    toastr.success('You have successfully subscribed to our newsletter!','Thank you!').css('width','400px');
+    toastr.success('You have successfully subscribed to our newsletter!','Thank you for your interest').css('width','400px');
     ;
 }
